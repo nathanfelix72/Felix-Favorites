623d58e243beb459fc95f6216d33e6da32ea5662
nathanfelix72 <nathan.felix@gmail.com>
2025-12-02 20:13:19 -0700

    Updates and refactor

---
+++ Nathan's Recipes/View/RecipeList/RecipeListRow.swift
@@ -0,0 +1,19 @@
+//
+//  RecipeListRow.swift
+//  Nathan's Recipes
+//
+//  Created by IS 543 on 12/2/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct RecipeListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListRow()
+}
